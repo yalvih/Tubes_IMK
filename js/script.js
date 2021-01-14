258e28da073d9f9ad785b490399fb12f112ae68e
yalvih <yalvi17@gmail.com>
2021-01-14 22:18:42 +0700

    besok lagi

--- js/script.js
+++ js/script.js
@@ -21,3 +21,16 @@ function showSlides(){
 
 // FormLogin
 
+var div_top = $('.menu').offset().top;
+
+$(window).scroll(function() {
+    var window_top = $(window).scrollTop() - 0;
+    if (window_top > div_top) {
+        if (!$('.menu').is('.sticky')) {
+            $('.menu').addClass('sticky');
+        }
+    } else {
+        $('.menu').removeClass('sticky');
+    }
+});
+
